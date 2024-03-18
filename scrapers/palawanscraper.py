659fc4215434f2d3e06dcb61a36ea8f238ae92e3
BBBoyu <wendengboyu2020@gmail.com>
2024-03-18 15:51:57 +0800

    palawan scrapers done and data uploaded

--- scrapers/palawanscraper.py
+++ scrapers/palawanscraper.py
@@ -1,11 +1,20 @@
+import time
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from datetime import date,timedelta
+from datetime import date
+from selenium.webdriver.chrome.options import Options
+import os
+from supabase import create_client, Client
+from dotenv import load_dotenv
 
-url = 'https://www.thepalawansentosa.com/attractions-dining/'
+#url = 'https://www.thepalawansentosa.com/attractions-dining/'
 
+chrome_options = Options()
+chrome_options.add_argument("--headless")
 driver = webdriver.Chrome()
 
 def get(link, xpath):
@@ -26,7 +35,7 @@ def _extract_all_hyperlinks(hyperlink):
     return href_links
 
 
-href_links = _extract_all_hyperlinks(url)
+href_links = _extract_all_hyperlinks('https://www.thepalawansentosa.com/attractions-dining/')
 
 
 
@@ -34,45 +43,89 @@ href_links = _extract_all_hyperlinks(url)
 
 hydrodash_url = 'https://hydrodash-online.globaltix.com/'
 
-def _extract_hydrodash_product(link):
+def _extract_hydrodash_product(url):
     hydro_data = []
-    hydro_data.extend(_extract_hydrodash_single(link))
-    hydro_data.extend(_extract_hydrodash_bundles(link))
+    driver.get(url)
+    time.sleep(5)
+    hydro_data.extend(_extract_ultragolf_single())
+    driver.get(url)
+    time.sleep(5)
+    hydro_data.extend(_extract_ultragolf_bundles_drink())
+    driver.get(url)
+    time.sleep(5)
+    hydro_data.extend(_extract_ultragolf_bundles_food())
     return hydro_data
 
-def _extract_hydrodash_single(url):
+def _extract_hydrodash_single():
     data = []
-    driver.get(url)
-    driver.find_elements(by=By.XPATH, value='/html/body/main/div[2]/div/section/div[3]/div[1]/div/div[2]/div[1]')[0].click()
-    driver.implicitly_wait(5)
-    name = driver.find_elements(By.CLASS_NAME, 'variation-name')
-    price = driver.find_elements(By.CLASS_NAME, 'price-strong-normal')
-    namelst = ([x.text for x in name])
-    agelst = ['Per Person (6 Years Old And Above)'] * 3
-    pricelst = ([x.text for x in price])
-    for i in range(len(namelst)):
-        dic = {}
-        dic['product'] = namelst[i]
-        dic['age group'] = agelst[i]
-        dic['price'] = pricelst[i]
-        data.append(dic)
+    category = "attractions"
+    subcategory = "HydroDash"
+    url = 'https://hydrodash-online.globaltix.com/'
+    image = driver.find_elements(By.CSS_SELECTOR, 'div.col-12.p-0.card_image_container')[0].find_element(By.TAG_NAME,"img").get_attribute("src")
+    driver.find_element(By.CLASS_NAME, 'list-price').click()
+    description = driver.find_element(By.CLASS_NAME, 'product-description').get_attribute('innerText')
+    for i in range(2):
+        data.append({
+            "company" : "thepalawansentosa",
+            "product_name" : "HydroDash " + driver.find_elements(By.CLASS_NAME, 'variation-name')[i].text,
+            "price" : driver.find_elements(By.CLASS_NAME, 'price-strong-normal')[i].text,
+            "scrapped_at" : date.today(),
+            "description" : description,
+            "source_link" : url,
+            "remarks" : driver.find_elements(By.CSS_SELECTOR, 'div.card-body.px-3.py-3.ticket-details-box.mb-3')[1].get_attribute('innerText'),
+            "image" : image,
+            "category": category,
+            "subcategory" : subcategory
+        })
+    time.sleep(3)
     return data
 
-def _extract_hydrodash_bundles(url):
+def _extract_hydrodash_drink():
     data = []
-    links = ['/html/body/main/div[3]/div/div[3]/div[1]/div/div[2]', '/html/body/main/div[3]/div/div[3]/div[2]/div/div[2]']
-    for link in links:
-        driver.get(url)
-        dic = {}
-        driver.find_elements(by=By.XPATH, value=link)[0].click()
-        driver.implicitly_wait(10)
-        name = driver.find_elements(By.CLASS_NAME, 'variation-name')
-        age = driver.find_elements(by=By.XPATH, value='/html/body/main/div[3]/div/div[2]/div[1]/div/div/div[2]/div/div[1]/div/div[1]/div[2]/form/div/div[1]/p/strong')
-        price = driver.find_elements(by=By.XPATH, value='/html/body/main/div[3]/div/div[2]/div[1]/div/div/div[2]/div/div[1]/div/div[1]/div[2]/form/div/div[2]/div/div[2]/strong')
-        dic['product'] = [x.text for x in name][0]
-        dic['age group'] = [x.text for x in age][0]
-        dic['price'] = [x.text for x in price][0]
-        data.append(dic)
+    category = "attractions"
+    subcategory = "HydroDash"
+    url = 'https://hydrodash-online.globaltix.com/'
+    description = driver.find_element(By.CSS_SELECTOR, 'div.group.inner.list-group-item-text.product-description.justify').get_attribute('innerText')
+    image = driver.find_elements(By.CSS_SELECTOR, 'div.col-12.p-0.card_image_container')[4].find_element(By.TAG_NAME,"img").get_attribute("src")
+    driver.find_elements(By.CLASS_NAME, 'list-price')[4].click()
+    time.sleep(3)
+    driver.find_elements(By.CSS_SELECTOR, 'div.row.ticket-card')[0].click()
+    data.append({
+        "company" : "thepalawansentosa",
+        "product_name" : driver.find_element(By.CSS_SELECTOR, 'h2.row-title.mb-0.row-title-md').get_attribute("innerText"),
+        "price" : driver.find_element(By.CSS_SELECTOR, 'div.col-12.price.px-4.px-md-0.text-left.text-md-right').find_elements(By.TAG_NAME, 'strong')[0].get_attribute("innerText"),
+        "scrapped_at" : date.today(),
+        "description" : description,
+        "source_link" : url,
+        "remarks" : driver.find_elements(By.CLASS_NAME, 'viewmore-card')[2].get_attribute('innerText'),
+        "image" : image,
+        "category": category,
+        "subcategory" : subcategory
+        })
+    return data
+
+def _extract_hydrodash_food():
+    data = []
+    category = "attractions"
+    subcategory = "HydroDash"
+    url = 'https://hydrodash-online.globaltix.com/'
+    description = driver.find_element(By.CSS_SELECTOR, 'div.group.inner.list-group-item-text.product-description.justify').get_attribute('innerText')
+    image = driver.find_elements(By.CSS_SELECTOR, 'div.col-12.p-0.card_image_container')[5].find_element(By.TAG_NAME,"img").get_attribute("src")
+    driver.find_elements(By.CLASS_NAME, 'list-price')[5].click()
+    time.sleep(3)
+    driver.find_elements(By.CSS_SELECTOR, 'div.row.ticket-card')[0].click()
+    data.append({
+        "company" : "thepalawansentosa",
+        "product_name" : driver.find_element(By.CSS_SELECTOR, 'h2.row-title.mb-0.row-title-md').get_attribute("innerText"),
+        "price" : driver.find_element(By.CSS_SELECTOR, 'div.col-12.price.px-4.px-md-0.text-left.text-md-right').find_elements(By.TAG_NAME, 'strong')[0].get_attribute("innerText"),
+        "scrapped_at" : date.today(),
+        "description" : description,
+        "source_link" : url,
+        "remarks" : driver.find_elements(By.CLASS_NAME, 'viewmore-card')[2].get_attribute('innerText'),
+        "image" : image,
+        "category": category,
+        "subcategory" : subcategory
+        })
     return data
 
 """# Hyperdrive"""
@@ -83,23 +136,35 @@ hyperdrive_xpathlst = ['/html/body/div[2]/div[3]/div[2]/div[1]', '/html/body/div
 def _extract_hyperdrive_product(link, xpath):
     data = []
     driver.get(link)
+    category = "attractions"
+    subcategory = "Hyperdrive"
+    url = 'https://www.apex-timing.com/gokarts/sessions_booking.php?center=559'
     name = driver.find_elements(By.CLASS_NAME, 'title')
     price = driver.find_elements(By.CLASS_NAME, 'value')
+    description = driver.find_elements(By.CLASS_NAME, 'description')
     for path in xpath:
         dic = {}
         driver.find_element(by=By.XPATH, value=path).click()
-        driver.implicitly_wait(5)
+        time.sleep(3)
         namelst = ([x.text for x in name if x != 'ONLINE BOOKING' and x != 'Use a voucher:' and x != ''])
-        agelst = ['9 Years Old And Above'] * 3
         pricelst = ([x.text for x in price if x != ''])
+        descriptionlst = ([x.text for x in description if x != ''])
         filtered_namelst = list(filter(lambda x: x != 'ONLINE BOOKING' and x != 'Use a voucher:' and x != '', namelst))
         filtered_pricelst = list(filter(lambda x: x != 'ONLINE BOOKING' and x != 'Use a voucher:' and x != '', pricelst))
+        filtered_descriptionlst = list(filter(lambda x: x != 'ONLINE BOOKING' and x != 'Use a voucher:' and x != '', descriptionlst))
         for i in range(len(filtered_namelst)):
-            dic = {}
-            dic['product'] = filtered_namelst[i]
-            dic['age group'] = agelst[i]
-            dic['price'] = filtered_pricelst[i]
-            data.append(dic)
+            data.append({
+                "company" : "thepalawansentosa",
+                "product_name" : filtered_namelst[i],
+                "price" : filtered_pricelst[i],
+                "scrapped_at" : date.today(),
+                "description" : filtered_descriptionlst[i],
+                "source_link" : url,
+                "remarks" : None,
+                "image" : None,
+                "category": category,
+                "subcategory" : subcategory
+                })
     return data
 
 
@@ -107,61 +172,116 @@ def _extract_hyperdrive_product(link, xpath):
 
 ultragolf_url = 'https://ultragolf-online.globaltix.com/'
 
-def _extract_ultragolf_product(link):
+def _extract_ultragolf_product(url):
     ultra_data = []
-    ultra_data.extend(_extract_ultragolf_single(link))
-    ultra_data.extend(_add_ultra_child_data())
-    ultra_data.extend(_extract_ultragolf_bundles(link))
+    driver.get(url)
+    time.sleep(5)
+    ultra_data.extend(_extract_ultragolf_single())
+    driver.get(url)
+    time.sleep(5)
+    ultra_data.extend(_extract_ultragolf_bundles_drink())
+    driver.get(url)
+    time.sleep(5)
+    ultra_data.extend(_extract_ultragolf_bundles_food())
     return ultra_data
 
-def _extract_ultragolf_single(url):
+def _extract_ultragolf_single():
     data = []
-    driver.get(url)
-    driver.find_elements(by=By.XPATH, value='/html/body/main/div[2]/div/section/div[3]/div[1]/div/div[2]/div[1]')[0].click()
-    driver.implicitly_wait(5)
-    name = driver.find_elements(By.CLASS_NAME, 'variation-name')
-    price = driver.find_elements(By.CLASS_NAME, 'price-strong-normal')
-    namelst = ([x.text for x in name])
-    agelst = ['Adult'] * 2
-    pricelst = ([x.text for x in price])
-    for i in range(len(namelst)):
-        dic = {}
-        dic['product'] = namelst[i]
-        dic['age group'] = agelst[i]
-        dic['price'] = pricelst[i]
-        data.append(dic)
+    category = "attractions"
+    subcategory = "UltraGolf"
+    url = 'https://ultragolf-online.globaltix.com/'
+    image = driver.find_element(By.CSS_SELECTOR, 'div.col-12.p-0.card_image_container').find_element(By.TAG_NAME,"img").get_attribute("src")
+    driver.find_element(By.CLASS_NAME, 'list-price').click()
+    description = driver.find_element(By.CLASS_NAME, 'product-description').get_attribute('innerText')
+    time.sleep(3)
+    driver.find_elements(By.CSS_SELECTOR, 'div.row.ticket-card')[0].click()
+    data.append({
+        "company" : "thepalawansentosa",
+        "product_name" : driver.find_element(By.CSS_SELECTOR, 'h2.row-title.mb-0.row-title-md.pr-5').get_attribute("innerText"),
+        "price" : driver.find_element(By.CSS_SELECTOR, 'div.col-12.price.px-4.px-md-0.text-left.text-md-right').find_elements(By.TAG_NAME, 'strong')[0].get_attribute("innerText"),
+        "scrapped_at" : date.today(),
+        "description" : description,
+        "source_link" : url,
+        "remarks" : driver.find_elements(By.CLASS_NAME, 'viewmore-card')[1].get_attribute('innerText'),
+        "image" : image,
+        "category": category,
+        "subcategory" : subcategory
+        })
     return data
 
 
-def _add_ultra_child_data():
-    child_data = []
-    namelst = ['Admission Ticket','Admission Ticket with Weather Inclement Insurance']
-    pricelst = ['SGD 18.00', 'SGD 20.00']
-    for i in range(2):
-        dic = {}
-        dic['product'] = namelst[i]
-        dic['age group'] = 'Child'
-        dic['price'] = pricelst[i]
-        child_data.append(dic)
-    return child_data
-
-def _extract_ultragolf_bundles(url):
+def _extract_ultragolf_bundles_drink():
     data = []
-    links = ['/html/body/main/div[3]/div/div[3]/div[1]/div/div[2]', '/html/body/main/div[3]/div/div[3]/div[2]/div/div[2]']
-    agexpath = ['/html/body/main/div[3]/div/div[2]/div[1]/div/div/div[2]/div/div[1]/div/div[1]/div[2]/form[1]/div/div[1]/p/strong', '/html/body/main/div[3]/div/div[2]/div[1]/div/div/div[2]/div/div[1]/div/div[1]/div[2]/form[2]/div/div[1]/p/strong']
-    pricexpath = ['/html/body/main/div[3]/div/div[2]/div[1]/div/div/div[2]/div/div[1]/div/div[1]/div[2]/form[1]/div/div[2]/div/div[2]/strong', '/html/body/main/div[3]/div/div[2]/div[1]/div/div/div[2]/div/div[1]/div/div[1]/div[2]/form[2]/div/div[2]/div/div[2]/strong']
-    for i in range(len(links)):
-        driver.get(url)
-        driver.find_elements(by=By.XPATH, value=links[i])[0].click()
-        driver.implicitly_wait(30)
-        for j in range(len(agexpath)):
-            dic = {}
-            name = driver.find_elements(By.CLASS_NAME, 'variation-name')
-            age = driver.find_elements(by=By.XPATH, value = agexpath[j])
-            price = driver.find_elements(by=By.XPATH, value = pricexpath[j])
-            dic['product'] = [x.text for x in name][0]
-            dic['age group'] = [x.text for x in age][0]
-            dic['price'] = [x.text for x in price][0]
-            data.append(dic)
+    category = "attractions"
+    subcategory = "UltraGolf"
+    url = 'https://ultragolf-online.globaltix.com/'
+    description = driver.find_element(By.CSS_SELECTOR, 'div.group.inner.list-group-item-text.product-description.justify').get_attribute('innerText')
+    image = driver.find_elements(By.CSS_SELECTOR, 'div.col-12.p-0.card_image_container')[4].find_element(By.TAG_NAME,"img").get_attribute("src")
+    driver.find_elements(By.CLASS_NAME, 'list-price')[4].click()
+    time.sleep(3)
+    driver.find_elements(By.CSS_SELECTOR, 'div.row.ticket-card')[0].click()
+    data.append({
+        "company" : "thepalawansentosa",
+        "product_name" : driver.find_element(By.CSS_SELECTOR, 'h2.row-title.mb-0.row-title-md').get_attribute("innerText"),
+        "price" : driver.find_element(By.CSS_SELECTOR, 'div.col-12.price.px-4.px-md-0.text-left.text-md-right').find_elements(By.TAG_NAME, 'strong')[0].get_attribute("innerText"),
+        "scrapped_at" : date.today(),
+        "description" : description,
+        "source_link" : url,
+        "remarks" : driver.find_elements(By.CLASS_NAME, 'viewmore-card')[2].get_attribute('innerText'),
+        "image" : image,
+        "category": category,
+        "subcategory" : subcategory
+        })
     return data
-#need debug
+
+
+def _extract_ultragolf_bundles_food():
+    data = []
+    category = "attractions"
+    subcategory = "UltraGolf"
+    url = 'https://ultragolf-online.globaltix.com/'
+    description = driver.find_element(By.CSS_SELECTOR, 'div.group.inner.list-group-item-text.product-description.justify').get_attribute('innerText')
+    image = driver.find_elements(By.CSS_SELECTOR, 'div.col-12.p-0.card_image_container')[5].find_element(By.TAG_NAME,"img").get_attribute("src")
+    driver.find_elements(By.CLASS_NAME, 'list-price')[5].click()
+    time.sleep(3)
+    driver.find_elements(By.CSS_SELECTOR, 'div.row.ticket-card')[0].click()
+    data.append({
+        "company" : "thepalawansentosa",
+        "product_name" : driver.find_element(By.CSS_SELECTOR, 'h2.row-title.mb-0.row-title-md').get_attribute("innerText"),
+        "price" : driver.find_element(By.CSS_SELECTOR, 'div.col-12.price.px-4.px-md-0.text-left.text-md-right').find_elements(By.TAG_NAME, 'strong')[0].get_attribute("innerText"),
+        "scrapped_at" : date.today(),
+        "description" : description,
+        "source_link" : url,
+        "remarks" : driver.find_elements(By.CLASS_NAME, 'viewmore-card')[2].get_attribute('innerText'),
+        "image" : image,
+        "category": category,
+        "subcategory" : subcategory
+        })
+    return data
+
+
+hydrodash_product = _extract_hydrodash_product(hydrodash_url)
+hyperdrive_product = _extract_hyperdrive_product(hyperdrive_url, hyperdrive_xpathlst)
+ultragolf_product = _extract_ultragolf_product(ultragolf_url)
+data_collected = []
+data_collected.extend(hydrodash_product)
+data_collected.extend(hyperdrive_product)
+data_collected.extend(ultragolf_product)
+
+print(data_collected)
+
+
+#Further cleaning
+def further_cleaning():
+    for d in data_collected:
+        d["scrapped_at"] = d["scrapped_at"].isoformat()
+        d["price"] = float(''.join([c for c in d["price"] if c.isdigit() or c == '.']))
+
+further_cleaning()
+
+# Inserting data into Supabase
+load_dotenv()
+url: str = os.environ.get("SUPABASE_URL")
+key: str = os.environ.get("SUPABASE_KEY")
+supabase: Client = create_client(url, key)
+data, count = supabase.table("sc_products").insert(data_collected).execute()
