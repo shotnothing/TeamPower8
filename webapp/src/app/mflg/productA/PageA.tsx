592d919d19ac4b8618d101756eee1ef1d7b66557
kiahmin <kiahminh@gmail.com>
2024-03-25 00:48:34 +0800

    Wrote tsx for productA but page cannot be found

--- webapp/src/app/mflg/productA/PageA.tsx
+++ webapp/src/app/mflg/productA/PageA.tsx
@@ -4,37 +4,53 @@ import "./CBB_Page.css"; // Import CSS file
 const BeachBazaarPage: React.FC = () => {
   return (
     <section className="wrap-3d">
-      <div className="item-3d">
-        <span className="ground"></span>
-        <figure className="item-content group">
-          <div className="item-img">
-            <img src="https://upload.wikimedia.org/wikipedia/commons/6/66/Breviceps-adspersus-adspersus.jpg" alt="" />
-          </div>
-          <figcaption className="item-caption">
-            <p>
-              <strong>Rain Frog</strong><br />
-              Can't Jump<br />
-              Can't Swim<br />
-              Only Screams<br />
-            </p>
-          </figcaption>
-        </figure>
-      </div>
-
-      <div className="item-3d">
-        <figure className="item-content group">
-          <figcaption className="item-caption">
-            <p>
-              <strong>Price bar here</strong><br />
-            </p>
-          </figcaption>
-        </figure>
-      </div>
-
-      <footer className="footer">
-        <p> footer <a href="" target="_blank"></a></p>
-      </footer>
-    </section>
+    <div className="item-3d">
+      <span className="ground" />
+      <figure className="item-content group">
+        <div className="item-img">
+          <img
+            alt=""
+            src="https://upload.wikimedia.org/wikipedia/commons/6/66/Breviceps-adspersus-adspersus.jpg"
+          />
+        </div>
+        <figcaption className="item-caption">
+          <p>
+            <strong>
+              Rain Frog
+            </strong>
+            <br />
+            Can't Jump
+            <br />
+            Can't Swim
+            <br />
+            Only Screams
+            <br />
+          </p>
+        </figcaption>
+      </figure>
+    </div>
+    <div className="item-3d">
+      <figure className="item-content group">
+        <figcaption className="item-caption">
+          <p>
+            <strong>
+              Price bar here
+            </strong>
+            <br />
+          </p>
+        </figcaption>
+      </figure>
+    </div>
+    <footer className="footer">
+      <p>
+        {' '}footer{' '}
+        <a
+          href=""
+          target="_blank"
+        />
+      </p>
+    </footer>
+  </section>
   );
 };
 
