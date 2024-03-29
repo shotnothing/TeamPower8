485e5f131ed4b5603223d4f3667ee0870dbbc04b
yizhenpong <yizhenpong@gmail.com>
2024-03-30 02:20:03 +0800

    render product list when search bar empty + without interacting with it
    previously rendered full list only if u typed sth in search bar then delete everything in the filter

--- webapp/src/app/home/components/search_bar.tsx
+++ webapp/src/app/home/components/search_bar.tsx
@@ -5,36 +5,10 @@ import './search_bar.css'
 
 // include relevant edits based on our defined API: https://github.com/shotnothing/TeamPower8/blob/main/docs/API.md
 
-const search_bar = ({setResults}) => {
-    const [input, set_input] = useState("");
-
-    // /product/all --> get product_name
-    // render all if nothing is typed in text box (don't have value && in return section of results) - confirm if filter is done frontend
-
-    const fetchData = (value) => {
-        fetch("https://jsonplaceholder.typicode.com/users").then((response) => response.json())
-        .then((json) => {
-            // console.log(json);
-
-            if (value === "") {
-                setResults(json); // Return all data
-                return;
-            }
-            const regex = new RegExp(value);
-            const results = json.filter((row_data) => {
-                return (
-                    row_data &&
-                    row_data.name &&
-                    regex.test(row_data.name)
-                  );
-            });
-            // console.log(results)
-            setResults(results)
-        });
-    };
-
+const search_bar = ({setInput, fetchData}) => {
+    
     const handleChange = (value) => {
-        set_input(value);
+        setInput(value);
         fetchData(value);
       };
 
@@ -43,7 +17,6 @@ const search_bar = ({setResults}) => {
             <FaSearch id="search-icon" />
             <input 
             placeholder='Type to search...' 
-            value={input}
             onChange={(e) => handleChange(e.target.value)}
             />
         </div>
--- webapp/src/app/home/page.tsx
+++ webapp/src/app/home/page.tsx
@@ -1,13 +1,42 @@
 "use client"
-import React, {useState}  from 'react';
+import React, {useState, useEffect}  from 'react';
 import './styles.modules.css';
 import Introduction from './components/introduction';
 import SearchBar from './components/search_bar';
 import ProductList from './components/product_list';
 
 const HomePage: React.FC = () => {
+    const [input, setInput] = useState("");
+    const [results, setResults] = useState([]);
 
-    const [results, setResults] = useState("");
+    useEffect(() => {
+        fetchData(input)
+    });
+
+    // /product/all --> get product_name
+    // render all if nothing is typed in text box (don't have value && in return section of results) - confirm if filter is done frontend
+
+    const fetchData = (value) => {
+        fetch("https://jsonplaceholder.typicode.com/users").then((response) => response.json())
+        .then((json) => {
+            // console.log(json);
+
+            if (value === "") {
+                setResults(json); // Return all data
+                return;
+            }
+            const regex = new RegExp(value);
+            const results = json.filter((row_data) => {
+                return (
+                    row_data &&
+                    row_data.name &&
+                    regex.test(row_data.name)
+                  );
+            });
+            // console.log(results)
+            setResults(results)
+        });
+    };
 
     return (
         <div className='home-page'>
@@ -20,7 +49,7 @@ const HomePage: React.FC = () => {
             <div className='search-bar-container'>
                 <h5>Please search for the MFLG product for which you would like to understand its competitors</h5>
                 {/* <SearchBar /> */}
-                <SearchBar setResults={setResults} />
+                <SearchBar setInput={setInput} fetchData={fetchData}/>
             </div>
 
             <div className='alert-dashboard'>
