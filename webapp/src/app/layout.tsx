5cd9638a5ce62a7992a74f2a2fc2e4e6fc894865
simin <e0775244@u.nus.edu>
2024-04-01 22:28:36 +0800

    Prevent access to pages before login

--- webapp/src/app/layout.tsx
+++ webapp/src/app/layout.tsx
@@ -1,8 +1,28 @@
 "use client"
+
 import 'bootstrap/dist/css/bootstrap.css';
-import './globals.css'
-import { SessionProvider } from "next-auth/react";
+import './globals.css';
+import { SessionProvider, useSession } from "next-auth/react";
+import { useRouter } from 'next/navigation';
 import { TopBar } from "@/app/topbar";
+
+const SessionLayout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+  const router = useRouter();
+  const { data: session } = useSession();
+
+  if (!session) {
+    router.push('/');
+  }
+
+  return (
+    <html lang="en">
+      <body>
+        <TopBar />
+        {children}
+      </body>
+    </html>
+  );
+};
 
 export default function RootLayout({
   children,
@@ -11,9 +31,7 @@ export default function RootLayout({
 }) {
   return (
     <SessionProvider>
-      <html lang="en">
-        <body><TopBar />{children}</body>
-      </html>
+      <SessionLayout>{children}</SessionLayout>
     </SessionProvider>
-  )
+  );
 }
