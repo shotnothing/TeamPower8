5e0a89c257323c3983976e1016b43bb05a26432b
simin <e0775244@u.nus.edu>
2024-04-01 20:54:44 +0800

    Login mechanism update
    Updated design of login page
    Moved log out button to topbar

--- webapp/src/app/layout.tsx
+++ webapp/src/app/layout.tsx
@@ -1,24 +1,8 @@
 "use client"
 import 'bootstrap/dist/css/bootstrap.css';
 import './globals.css'
-import Link from 'next/link';
-import Image from "next/image";
-import logo from '@/app/logo.png'
 import { SessionProvider } from "next-auth/react";
-
-export const TopBar = () => {
-    return (
-        <div className="topbar">
-            <div className="left">
-                <Image src={logo} width={35} height={35} alt="logo" className="logo" />
-                <span className="appname">PriceProbe</span>
-            </div>
-            <div className="right">
-                <Link href="/home">Home</Link>
-            </div>
-        </div>
-    )
-};
+import { TopBar } from "@/app/topbar";
 
 export default function RootLayout({
   children,
---
+++ webapp/src/app/topbar.tsx
@@ -0,0 +1,39 @@
+import { signOut, useSession } from 'next-auth/react';
+import Image from "next/image";
+import logo from '@/app/logo.png';
+import { useRouter } from 'next/navigation'; 
+
+export const TopBar = () => {
+    const router = useRouter();
+    const { data: session } = useSession();
+
+    const redirectToLoginPage = () => {
+        router.push('/');
+    };
+
+    const redirectToHomePage = () => {
+        router.push('/home');
+    };
+
+    const handleSignOut = async () => {
+        await signOut({ redirect: false });
+        redirectToLoginPage(); 
+    };
+
+    return (
+        <div className="topbar">
+            <div className="left">
+                <Image src={logo} width={35} height={35} alt="logo" className="logo" />
+                <span className="appname">PriceProbe</span>
+            </div>
+            <div className="right">
+                {session && 
+                <>
+                    <button type="button" className="btn" onClick={redirectToHomePage}>Home</button>
+                    <button type="button" className="btn btn-light" onClick={handleSignOut}>Sign out</button>
+                </>
+                }
+            </div>
+        </div>
+    );
+};
