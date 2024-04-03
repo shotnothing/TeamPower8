c18ac0e4cb587878a8f2c77e531584879de4cd94
simin <e0775244@u.nus.edu>
2024-04-03 15:05:02 +0800

    Google authorization update
    Prevent authorized google accounts from signing in

--- webapp/src/app/api/auth/[...nextauth]/route.ts
+++ webapp/src/app/api/auth/[...nextauth]/route.ts
@@ -9,6 +9,7 @@ const handler = NextAuth({
         GoogleProvider({
             clientId: process.env.GOOGLE_CLIENT_ID as string,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+            authorization: { params: { scope: 'email https://www.googleapis.com/auth/calendar'} },
         })
     ]
 })
