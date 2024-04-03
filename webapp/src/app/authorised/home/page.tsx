c67b6b816c25c71d3a75f6cf1d2e3b34df4e93eb
Jocelyn Yeo <106587991+Jocelyn2910@users.noreply.github.com>
2024-04-03 15:52:37 +0800

    Merge conflict

--- webapp/src/app/authorised/home/page.tsx
+++ webapp/src/app/authorised/home/page.tsx
@@ -3,13 +3,11 @@
 // }
 
 import React from 'react';
-import PriceBar from '../pricebar/page'; // Assuming PriceBar.js is in the same directory
 
 const HomePage = () => {
     return (
         <div>
             <h1>Welcome to the Home Page</h1>
-            <PriceBar />
         </div>
     );
 }
