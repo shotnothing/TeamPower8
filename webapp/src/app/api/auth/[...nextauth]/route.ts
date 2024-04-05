93158573c4851d339a523f5d8e5ddf62e116c6d5
simin <e0775244@u.nus.edu>
2024-04-05 17:08:24 +0800

    Update to include auth_secret
    solves jwt error

--- webapp/src/app/api/auth/[...nextauth]/route.ts
+++ webapp/src/app/api/auth/[...nextauth]/route.ts
@@ -5,6 +5,7 @@ import NextAuth from "next-auth"
 import GoogleProvider from "next-auth/providers/google";
 
 const handler = NextAuth({
+    secret: process.env.AUTH_SECRET,
     providers: [
         GoogleProvider({
             clientId: process.env.GOOGLE_CLIENT_ID as string,
