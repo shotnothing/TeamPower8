73fbabe6276c9caade2bbe07a598e72ac69cdba9
shotnothing <shotnothing@protonmail.ch>
2024-04-14 23:11:58 +0800

    fix(api): switching CORS middleware order

--- api/backend_server/settings.py
+++ api/backend_server/settings.py
@@ -27,17 +27,16 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
-
 # Application definition
 
 INSTALLED_APPS = [
-    'corsheaders',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -50,6 +49,8 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ORIGIN_ALLOW_ALL = True
 
 ROOT_URLCONF = 'backend_server.urls'
 
@@ -127,12 +128,6 @@ STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-CORS_ALLOWED_ORIGINS = [
-    'http://localhost:3000',
-    'http://localhost:81',
-    'http://localhost:80',
-]
-CORS_ORIGIN_ALLOW_ALL = True
 
 
 
